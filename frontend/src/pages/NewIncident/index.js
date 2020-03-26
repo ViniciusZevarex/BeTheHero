b646417642ccd28e1ed2599abfec39ff26db45ab
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2020-03-26 10:48:14 -0300

    Front-end com interaçao com os dados da api em node do back-end, finalizado.

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 import './styles.css';
 
+import api from '../../services/api';
+
 export default function Register(){
+    const ong_id = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    const [title,setTitle]              = useState('');
+    const [description, setDescription] = useState('');
+    const [value,setValue]              = useState('');
+
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        }
+
+        try{
+            await api.post('incidents', data, {
+                headers:{
+                    Authorization: ong_id,
+                }
+            });
+
+            history.push('/profile');
+        }catch(err){
+            alert('Erro ao cadastrar caso, tente novamente!!!');
+        }
+
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -22,10 +56,24 @@ export default function Register(){
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título do Caso" />
-                    <textarea placeholder="Descrição" />
-                    <input placeholder="Valor em R$" />
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Título do Caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+
+                    <textarea 
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+
+                    <input 
+                        placeholder="Valor em R$" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button  className="button" type="submit">Cadastrar</button>
                 </form>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 import './styles.css';
 
+import api from '../../services/api';
+
 export default function Register(){
+    const history = useHistory();
+    const [name, setName]           = useState('');
+    const [email, setEmail]         = useState('');
+    const [whatsapp, setWhatsapp]   = useState('');
+    const [city, setCity]           = useState('');
+    const [uf, setUf]               = useState('');
+
+
+
+    async function handleRegister(e){
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        };
+
+        try{
+            const response = await api.post('ongs', data);
+            alert(`Seu id de acesso ${response.data.id}`);
+
+            history.push('/');
+        }catch(err){
+            alert('Erro no cadastro tente novamente!');
+        }
+       
+    }
+
+
+
     return (
         <div className="register-content">
             <div className="content">
@@ -22,13 +57,35 @@ export default function Register(){
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="Nome da ONG" 
+                        value={name}
+                        onChange={ e => setName(e.target.value)}
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="E-mail" 
+                        value={email}
+                        onChange={ e => setEmail(e.target.value)}
+                    />
+                    <input 
+                        placeholder="WhatsApp" 
+                        value={whatsapp}
+                        onChange={ e => setWhatsapp(e.target.value)}
+                    />
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="uf" style={{ width: 80 }} />
+                        <input 
+                            placeholder="Cidade" 
+                            value={city}
+                            onChange={ e => setCity(e.target.value)}
+                        />
+                        <input 
+                            placeholder="uf" 
+                            style={{ width: 80 }} 
+                            value={uf}
+                            onChange={ e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button  className="button" type="submit">Cadastrar</button>
